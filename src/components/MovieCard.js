5b75ba99df7006fd81e69a8241aba68ef0c02719
Vaishnavi <uw.vgaikwad24@gmail.com>
2025-03-24 13:57:57 -0700

    Added styling for rating and error handler for image

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,14 +2,31 @@ import React from "react";
 import "../styles.css";
 
 export default function MovieCard({ movie, key }) {
+  const handleImgError = (e) => {
+    e.target.src = "images/default.jpg";
+  };
+
+  const getRatingClass = (rating) => {
+    if (rating >= 8) return "rating-good";
+    else if (rating > 5 && rating < 8) return "rating-ok";
+
+    return "rating-bad";
+  };
+
   return (
     <div>
       <div key={key} className="movie-card">
-        <img src={`images/${movie.image}`} alt={movie.title} />
+        <img
+          src={`images/${movie.image}`}
+          alt={movie.title}
+          onError={handleImgError}
+        />
         <div className="movie-card-info">
           <h3 className="movie-card-title">{movie.title}</h3>
           <p className="movie-card-genre">{movie.genre}</p>
-          <p className="movie-card-rating">{movie.rating}⭐</p>
+          <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+            {movie.rating}⭐
+          </p>
         </div>
       </div>
     </div>
