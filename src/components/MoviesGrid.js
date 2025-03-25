c45e5fb3285a806558b55fb8859f152644973cf3
Vaishnavi <uw.vgaikwad24@gmail.com>
2025-03-25 10:06:06 -0700

    Implemented search functionality

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,6 +4,7 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("movies.json")
@@ -11,11 +12,31 @@ export default function MoviesGrid() {
       .then((data) => setMovies(data));
   }, []);
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div class="movies-grid">
-      {movies.map((movie) => (
-        <MovieCard movie={movie} key={movie.id}></MovieCard>
-      ))}
+    <div>
+      <input
+        type="text"
+        placeholder="Search a movie..."
+        className="search-input"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div class="movies-grid">
+        {/* {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))} */}
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
     </div>
   );
 }
