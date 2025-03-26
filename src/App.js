36b94a7f7b609ae3b77bad2df42659bfe8ac8d56
Vaishnavi <uw.vgaikwad24@gmail.com>
2025-03-26 09:57:09 -0700

    Added Router to the application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles.css'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
+        {/* <MoviesGrid></MoviesGrid>
+        <Watchlist></Watchlist> */}
       </div>
       <Footer></Footer>
     </div>
